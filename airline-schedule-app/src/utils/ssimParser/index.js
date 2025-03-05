bd196478126c20ee26c12a70c90383f160746543
HE3HAU <eeggoorr211201@gmail.com>
2025-03-05 16:40:17 +0300

    v 0.0.1

--- airline-schedule-app/src/utils/ssimParser/index.js
+++ airline-schedule-app/src/utils/ssimParser/index.js
@@ -497,4 +497,5 @@ const calculateDuration = (departure, arrival) => {
   const minutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
   
   return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+
 };
